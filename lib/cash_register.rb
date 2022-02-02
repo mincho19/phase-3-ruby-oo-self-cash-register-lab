036a83e3746ebadc184066317904dc408b2817dd
Min Cho <minsangcho19@gmail.com>
2022-02-02 12:52:54 -0800

    commit tree 5d569e57e79ada8775164026ed263ac1d4646c75

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,42 @@
 
+class CashRegister
+    attr_accessor :total
+    attr_accessor :previoustotal
+    attr_reader :discount
+    attr_accessor :itemArray
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @itemArray = []
+    end
+
+    def add_item(title, price, optional = 1)
+        @previoustotal = self.total
+        self.total += (price*optional)
+        optional.times {self.itemArray << title}
+    end
+
+    def apply_discount()
+
+        if discount == 0
+            "There is no discount to apply."
+        else
+            self.total = self.total * (100-self.discount)/100
+            "After the discount, the total comes to $800."
+        end
+
+    end
+
+    def items
+        self.itemArray
+    end
+
+    def void_last_transaction
+        self.total = @previoustotal
+        self.itemArray.pop
+    end
+
+
+
+end
